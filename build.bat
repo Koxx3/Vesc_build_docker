524637dc95cee0d283cd70407a23f43b759b5403
Koxx3 <koxx33@gmail.com>
2022-04-15 22:53:31 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 docker build -t vesc_fw_503
-docker run --name "build_gigavesc" --rm -it -v ${pwd}\bldc:/bldc vesc_fw_503
-mv bldc/build/BLDC_4_ChibiOS.bin GigaVesc.bin
+docker run --name "build_gigavesc" --rm -it -v ${pwd}\bldc:/bldc vesc_fw bash
 
 pause
 
