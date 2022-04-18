10e8f31c4ccf2abff5bc2b2ae762c606197cd816
Koxx3 <koxx33@gmail.com>
2022-04-18 17:42:27 +0200

    improve

--- patch_nolimits.sh
+++ patch_nolimits.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sed -i '1s/^/#define DISABLE_HW_LIMITS\n/' ./bldc-503/conf_general.h
+sed -i 's|//#define DISABLE_HW_LIMITS.*|#define DISABLE_HW_LIMITS\n|' ./bldc-503/conf_general.h
+sed -i 's|//#define DISABLE_HW_LIMITS.*|#define DISABLE_HW_LIMITS\n|' ./bldc-master/conf_general.h
 
