8ecc366928ed7c34b441cc7c4252cd8e60312b71
Koxx3 <koxx33@gmail.com>
2022-11-14 16:34:10 +0100

    add hw_micro_v2

--- get_vesc.sh
+++ get_vesc.sh
@@ -5,6 +5,9 @@ git clone https://github.com/vedderb/bldc-bootloader /bldc-bootloader
 cp /bldc-spintend/hwconf/hw_ubox* /bldc-master/hwconf
 cp /bldc-spintend/hwconf/hw_ubox* /bldc-503/hwconf
 
+cp /other_hw/hw_micro* /bldc-master/hwconf
+cp /other_hw/hw_micro* /bldc-503/hwconf
+
 sed -i '1s/^/#define UBOX_V2_100V_250D\n/' /bldc-503/hwconf/hw_uboxv2_100_100.h
 sed -i '1s/^/#define UBOX_V2_100V_250D\n/' /bldc-master/hwconf/hw_uboxv2_100_100.h
 
