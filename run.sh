089a52c496cd2d609ac87bfacac1557cacce35e9
Koxx3 <koxx33@gmail.com>
2024-08-20 10:24:39 +0200

    change 605 release path

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 docker run --name vesc_fw --rm -it \
 -v $(pwd)/bldc-503:/bldc-503 \
--v $(pwd)/bldc-master:/bldc-master \
+-v $(pwd)/bldc-605:/bldc-605 \
+-v $(pwd)/other_hw:/other_hw \
  vesc_fw bash
 
