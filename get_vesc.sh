c957480fde2780d5e59065036109a33550759123
Koxx3 <koxx33@gmail.com>
2024-08-20 10:28:09 +0200

    Update get_vesc.sh

--- get_vesc.sh
+++ get_vesc.sh
@@ -1,15 +1,15 @@
-git clone -b master https://github.com/vedderb/bldc /bldc-master
+git clone -b release_6_05 https://github.com/vedderb/bldc /bldc-605
 git clone -b release_5_03 https://github.com/vedderb/bldc /bldc-503
-git clone https://github.com/JohnSpintend/bldc /bldc-spintend
-git clone https://github.com/vedderb/bldc-bootloader /bldc-bootloader
-cp /bldc-spintend/hwconf/hw_ubox* /bldc-master/hwconf
-cp /bldc-spintend/hwconf/hw_ubox* /bldc-503/hwconf
+# git clone https://github.com/JohnSpintend/bldc /bldc-spintend
+# git clone https://github.com/vedderb/bldc-bootloader /bldc-bootloader
+# cp /bldc-spintend/hwconf/hw_ubox* /bldc-master/hwconf
+# cp /bldc-spintend/hwconf/hw_ubox* /bldc-503/hwconf
 
-cp /other_hw/hw_micro* /bldc-master/hwconf
-cp /other_hw/hw_micro* /bldc-503/hwconf
+# cp /other_hw/hw_micro* /bldc-master/hwconf
+# cp /other_hw/hw_micro* /bldc-503/hwconf
 
-sed -i '1s/^/#define UBOX_V2_100V_250D\n/' /bldc-503/hwconf/hw_uboxv2_100_100.h
-sed -i '1s/^/#define UBOX_V2_100V_250D\n/' /bldc-master/hwconf/hw_uboxv2_100_100.h
+# sed -i '1s/^/#define UBOX_V2_100V_250D\n/' /bldc-503/hwconf/hw_uboxv2_100_100.h
+# sed -i '1s/^/#define UBOX_V2_100V_250D\n/' /bldc-master/hwconf/hw_uboxv2_100_100.h
 
-cd /bldc-master 
+cd /bldc-605
 make arm_sdk_install
